0f555c4a7591e06c1ca1c0e8cdfffe9b64aded9d
Renan Mestre <renanmestre2006@gmail.com>
2025-10-07 13:33:48 -0300

    Ajustes de códigos e segurança

--- src/main/java/com/api/Teste/User.java
+++ src/main/java/com/api/Teste/User.java
@@ -1,17 +1,24 @@
 package com.api.Teste;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users") // Evita conflitos com palavras reservadas como "user"
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true) // Garante unicidade do e-mail
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
     
